6b2b3ea708834def381e7a61b4950e2065e842ad
Richard James <richardjames2023@gmail.com>
2025-08-12 02:59:13 +0100

    Remove unused useEffect import in NewNavbar.jsx to fix build error

--- src/components/Global/NewNavbar.jsx
+++ src/components/Global/NewNavbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FaGlobe, FaBars, FaTimes } from 'react-icons/fa';
 import './NewNavbar.css';
